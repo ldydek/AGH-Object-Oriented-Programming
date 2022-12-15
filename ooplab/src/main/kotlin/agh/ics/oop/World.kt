f8de0d27ae8971d8c991b7cc9b236328584139d2
ldydek <lukaszdydek72@gmail.com>
2022-12-15 15:22:11 +0100

    do pkt. 21.

--- ooplab/src/main/kotlin/agh/ics/oop/World.kt
+++ ooplab/src/main/kotlin/agh/ics/oop/World.kt
@@ -1,16 +1,37 @@
 package agh.ics.oop
 
+
 fun main(args: Array<String>) {
-    println("system wystartował!")
-    run(arrayOf("f", "b", "l", "r"))
-    println("system zakończył działanie")
+    println("Start")
+    val output: MutableList<Direction> = fromEnumToString(args)
+    run(output)
+    println("Stop")
 }
 
-fun run(str: Array<String>) {
-    println("zwierzak idzie do przodu")
-    for (i in 0 until str.size-1) {
-        print(str[i])
-        print(", ")
+fun run(array: MutableList<Direction>) {
+    for (item in array) {
+        val value = when (item) {
+            Direction.FORWARD -> "idzie do przodu"
+            Direction.BACKWARD -> "idzie do tyłu"
+            Direction.LEFT -> "skręca w lewo"
+            Direction.RIGHT -> "skręca prawo"
+        }
+        if (value != "")
+            println("Zwierze $value")
     }
-    println(str[str.size-1])
+}
+
+fun fromEnumToString(args: Array<String>): MutableList<Direction> {
+    val output: MutableList<Direction> = ArrayList()
+    for (item in args) {
+        val value = when (item) {
+            "f" -> Direction.FORWARD
+            "b" -> Direction.BACKWARD
+            "l" -> Direction.LEFT
+            "r" -> Direction.RIGHT
+            else -> null
+        } ?: continue
+        output.add(value)
+    }
+    return output
 }
