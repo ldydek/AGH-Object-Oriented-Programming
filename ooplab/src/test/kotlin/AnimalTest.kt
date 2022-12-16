d5abb3839daa9502cf2e1abc70f1d6e57356c4db
ldydek <91955435+ldydek@users.noreply.github.com>
2022-12-16 15:56:55 +0100

    Update AnimalTest.kt

--- ooplab/src/test/kotlin/AnimalTest.kt
+++ ooplab/src/test/kotlin/AnimalTest.kt
@@ -38,19 +38,4 @@ class AnimalTest {
         animal.move(MoveDirection.FORWARD)
         assertEquals(Vector2d(2, 4), animal.getPosition())
     }
-
-    @Test
-    fun parsingTest() {
-        val strings = arrayOf("f", "forward", "b", "backward", "wrong", "left", "l", "wrong", "r", "wrong", "right")
-        val output: MutableList<MoveDirection> = OptionParser().parse(strings)
-        assertEquals(strings.size-3, output.size)
-        assertEquals(output[0], MoveDirection.FORWARD)
-        assertEquals(output[1], MoveDirection.FORWARD)
-        assertEquals(output[2], MoveDirection.BACKWARD)
-        assertEquals(output[3], MoveDirection.BACKWARD)
-        assertEquals(output[4], MoveDirection.LEFT)
-        assertEquals(output[5], MoveDirection.LEFT)
-        assertEquals(output[6], MoveDirection.RIGHT)
-        assertEquals(output[7], MoveDirection.RIGHT)
-    }
 }
