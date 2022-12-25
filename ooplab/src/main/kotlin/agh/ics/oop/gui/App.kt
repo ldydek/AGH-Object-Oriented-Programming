04c1972e48ee120b8917bd0cba81fff22e9bd67c
ldydek <lukaszdydek72@gmail.com>
2022-12-25 23:35:21 +0100

    still working

--- ooplab/src/main/kotlin/agh/ics/oop/gui/App.kt
+++ ooplab/src/main/kotlin/agh/ics/oop/gui/App.kt
@@ -14,29 +14,25 @@ import javafx.stage.Stage
 class App : Application() {
 
     private val map = GrassField(10)
-    private var numCols: Int = Int.MAX_VALUE
-    private var numRows: Int = Int.MAX_VALUE
     private val sceneRatio: Int = 75
-    private val corners = map.getCorners()
-    private val left: Int = corners[0].x
-    private val down: Int = corners[0].y
-    private val right: Int = corners[1].x
-    private val up: Int = corners[1].y
+    private val cornersCoordinates = map.getCorners()
+//    bottom left and upper right corners of map
 
     override fun init() {
         val args = parameters.raw
         println(args)
         val directions: ArrayList<MoveDirection> = OptionParser().parse(args.toTypedArray())
-        val positions = arrayOf(Vector2d(4, 3), Vector2d(1, 2))
+        val positions = arrayOf(Vector2d(10, 3), Vector2d(10, 2))
         val engine: IEngine = SimulationEngine(directions, map, positions)
         engine.run()
         println(map)
     }
 
     override fun start(primaryStage: Stage?) {
+        val (rowsNumber, columnsNumber) = countRowColumnQuantity()
         val gridPane: GridPane = createGridPane()
-        addGridPaneElements(gridPane, left)
-        val scene = Scene(gridPane, (numCols * sceneRatio).toDouble(), (numRows * sceneRatio).toDouble())
+        addGridPaneElements(gridPane, cornersCoordinates[0].x, cornersCoordinates[1].y)
+        val scene = Scene(gridPane, (columnsNumber * sceneRatio).toDouble(), (rowsNumber * sceneRatio).toDouble())
         if (primaryStage != null) {
             primaryStage.scene = scene
         }
@@ -47,62 +43,71 @@ class App : Application() {
         val gridPane = GridPane()
         gridPane.isGridLinesVisible = true
 
-        this.numRows = up - down + 1
-        this.numCols = right - left + 1
-
+        val (rowsNumber, columnsNumber) = countRowColumnQuantity()
         val yx = Label("y\\x")
         labelStyling(yx)
         gridPane.add(yx, 0, 0)
         GridPane.setHalignment(yx, HPos.CENTER)
 
-        for (i in 0 until numCols+1) {
+        for (i in 0 until columnsNumber+1) {
             val colConst = ColumnConstraints()
-            colConst.percentWidth = 100.0 / numCols
+            colConst.percentWidth = 100.0 / columnsNumber
             gridPane.columnConstraints.add(colConst)
         }
-        for (i in 0 until numRows+1) {
+        for (i in 0 until rowsNumber+1) {
             val rowConst = RowConstraints()
-            rowConst.percentHeight = 100.0 / numRows
+            rowConst.percentHeight = 100.0 / rowsNumber
             gridPane.rowConstraints.add(rowConst)
         }
-        addCoordinates(gridPane, left, right, down, up)
+        addCoordinates(gridPane)
         return gridPane
     }
 
-    private fun addCoordinates(gridPane: GridPane, left: Int, right: Int, down: Int, up: Int) {
-        println(numRows)
-        println(numCols)
-        println(up)
-        println(down)
-        for (i in up downTo down-1) {
+    private fun addCoordinates(gridPane: GridPane) {
+        val left = cornersCoordinates[0].x
+        val bottom = cornersCoordinates[0].y
+        val right = cornersCoordinates[1].x
+        val top = cornersCoordinates[1].y
+
+//        adding rows
+        var index = 0
+        for (i in top downTo bottom-1) {
+            index++
             val label = Label((i).toString())
             labelStyling(label)
-            gridPane.add(label, 0, numRows-i)
+            gridPane.add(label, 0, index)
             GridPane.setHalignment(label, HPos.CENTER)
         }
-        var counter = 0
+
+//        adding columns
+        index = 0
         for (i in left until right+1) {
-            counter++
+            index++
             val label = Label((i).toString())
             labelStyling(label)
-            gridPane.add(label, counter, 0)
+            gridPane.add(label, index, 0)
             GridPane.setHalignment(label, HPos.CENTER)
         }
     }
 
-    private fun addGridPaneElements(gridPane: GridPane, left: Int) {
+    private fun addGridPaneElements(gridPane: GridPane, left: Int, top: Int) {
         val mapElementList = map.getMapElementsListCopy()
         mapElementList.forEach {
             val label = Label(map.objectAt(it.getPosition()).toString())
             labelStyling(label)
             GridPane.setHalignment(label, HPos.CENTER)
-            gridPane.add(label, it.getPosition().x-left+1, numRows-it.getPosition().y)
+            gridPane.add(label, it.getPosition().x-left+1, top-it.getPosition().y+1)
         }
-        map.getMapElementsListCopy().forEach { println(it.getPosition()) }
     }
 
     private fun labelStyling(label: Label) {
         label.style = "-fx-font-weight: bold"
         label.style = "-fx-font-size: 40px"
+    }
+
+    private fun countRowColumnQuantity(): Array<Int> {
+        val rowsNumber = cornersCoordinates[1].y - cornersCoordinates[0].y + 1
+        val columnsNumber = cornersCoordinates[1].x - cornersCoordinates[0].x + 1
+        return arrayOf(rowsNumber, columnsNumber)
     }
 }
